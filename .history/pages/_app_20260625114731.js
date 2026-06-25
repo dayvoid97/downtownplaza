e635b041d894433dad147d9799911530890a5e66
dayvoid97 <sharma.kanchan3154@gmail.com>
2026-06-25 13:17:57 -0500

    added vercen ana

--- .history/pages/_app_20260625114731.js
+++ .history/pages/_app_20260625114731.js
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import { CartProvider } from '../lib/CartContext'
-import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
       <Component {...pageProps} />
-      <Analytics />
     </CartProvider>
   )
 }
